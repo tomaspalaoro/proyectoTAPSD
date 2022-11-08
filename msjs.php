95059c9bba91610490c2686abc886a77b9f1c046
Javier Vinal <98608731+JavierVICO@users.noreply.github.com>
2022-11-08 11:33:29 +0100

    añadido Modales Crear Evento,Actualizar evento y Mensaje Create/Delete

---
+++ msjs.php
@@ -0,0 +1,28 @@
+
+<?php
+if(isset($_REQUEST['e'])){ ?>
+<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+   El evento fue registrado correctamente.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<?php } ?>
+
+<?php
+if(isset($_REQUEST['ea'])){ ?>
+<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+   El evento fue actualizado correctamente.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<?php } ?>
+
+<div class="alert alert-danger alert-dismissible fade show text-center" role="alert" style="display: none;">
+   El evento fue borrado correctamente.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
