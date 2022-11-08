f3121f308e9f3d82505cf3ddfba0a6a781ea30c0
Javier Vinal <98608731+JavierVICO@users.noreply.github.com>
2022-11-08 11:30:07 +0100

    añadido Php añadir eventos

---
+++ nuevoEvento.php
@@ -0,0 +1,38 @@
+<?php
+require("Evento.php");
+date_default_timezone_set("Europe/Paris");
+setlocale(LC_ALL,"es_ES");
+//$hora = date("g:i:A");
+
+$evento            = ucwords($_REQUEST['evento']);
+$f_inicio          = $_REQUEST['fecha_inicio'];
+$fecha_inicio      = date('Y-m-d', strtotime($f_inicio)); 
+
+$f_fin             = $_REQUEST['fecha_fin']; 
+$seteando_f_final  = date('Y-m-d', strtotime($f_fin));  
+$fecha_fin1        = strtotime($seteando_f_final."+ 1 days");
+$fecha_fin         = date('Y-m-d', ($fecha_fin1));  
+$color_evento      = $_REQUEST['color_evento'];
+
+
+$InsertNuevoEvento = "INSERT INTO calendario(
+      evento,
+      fecha_inicio,
+      fecha_fin,
+      color_evento
+      )
+    VALUES (
+      '" .$evento. "',
+      '". $fecha_inicio."',
+      '" .$fecha_fin. "',
+      '" .$color_evento. "'
+  )";
+  $Singleton=Singleton::getInstance();
+
+  $stmt=$Singleton->prepare($InsertNuevoEvento);
+  //$sth=$con->prepare($InsertNuevoEvento);
+  $stmt->execute();
+
+header("Location:index.php?e=1");
+
+?>
