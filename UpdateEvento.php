8208c1e742dff92ae8c37c5f7f0a38bcc51c8795
Javier Vinal <98608731+JavierVICO@users.noreply.github.com>
2022-11-08 11:31:49 +0100

    Añadido Actualizar eventos

---
+++ UpdateEvento.php
@@ -0,0 +1,37 @@
+<?php
+date_default_timezone_set("Europe/Paris");
+setlocale(LC_ALL,"es_ES");
+
+require_once('Evento.php');
+$id='';
+$id=$_POST['idEvento'];
+//echo "id:". $id;
+
+$evento            = ucwords($_REQUEST['evento']);
+$f_inicio          = $_REQUEST['fecha_inicio'];
+$fecha_inicio      = date('Y-m-d', strtotime($f_inicio)); 
+
+$f_fin             = $_REQUEST['fecha_fin']; 
+$seteando_f_final  = date('Y-m-d', strtotime($f_fin));  
+$fecha_fin1        = strtotime($seteando_f_final."+ 1 days");
+$fecha_fin         = date('Y-m-d', ($fecha_fin1));  
+$color_evento      = $_REQUEST['color_evento'];
+
+//print_r($_REQUEST);
+$UpdateProd = ("UPDATE calendario 
+    SET evento ='$evento',
+        fecha_inicio ='$fecha_inicio',
+        fecha_fin ='$fecha_fin',
+        color_evento ='$color_evento'
+    WHERE id='".$id."' ");
+
+
+
+  $Singleton=Singleton::getInstance();
+  $stmt=$Singleton->prepare($UpdateProd);
+  $stmt->execute();
+  
+
+header("Location:index.php?ea=1");
+exit;
+?>
