d09553631ef36e5c922001a5f1b2b0a7bfd4ad9e
Tomas <tomaspalaoro@elcampico.org>
2022-11-10 19:40:24 +0100

    defined check en variables inc
    ahorra errores de variables ya definidas

--- PHP/variables.inc.php
+++ PHP/variables.inc.php
@@ -2,19 +2,20 @@
 
 /**********CONSTANTES PARA CAMBIAR*******/
 
-const DB_NAME = 'tapsd';
-const url_base = "http://localhost/";
+if (!defined('DB_NAME')) define('DB_NAME',"tapsd");
+if (!defined('url_base')) define('url_base',"http://localhost/");
 
 /******************************************/
 
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASS = '';
-const ruta_admin = url_base."proyectoTAPSD/admin.php";
-const ruta_login = url_base."proyectoTAPSD/login.php";
-const ruta_index = url_base."proyectoTAPSD/index.php";
-const ruta_tecnico = url_base."proyectoTAPSD/tecnico.php";
-const ruta_logout = url_base."proyectoTAPSD/PHP/logout.php";
-const ruta_registrar = url_base."proyectoTAPSD/PHP/registrar.php";
-const ruta_verificar = url_base."proyectoTAPSD/PHP/verificar.php";
+if (!defined('DB_HOST')) define('DB_HOST',"localhost");
+if (!defined('DB_USER')) define('DB_USER',"root");
+if (!defined('DB_PASS')) define('DB_PASS',"");
+
+if (!defined('ruta_admin')) define('ruta_admin',url_base."proyectoTAPSD/admin.php");
+if (!defined('ruta_login')) define('ruta_login',url_base."proyectoTAPSD/login.php");
+if (!defined('ruta_index')) define('ruta_index',url_base."proyectoTAPSD/index.php");
+if (!defined('ruta_tecnico')) define('ruta_tecnico',url_base."proyectoTAPSD/tecnico.php");
+if (!defined('ruta_logout')) define('ruta_logout',url_base."proyectoTAPSD/PHP/logout.php");
+if (!defined('ruta_registrar')) define('ruta_registrar',url_base."proyectoTAPSD/PHP/registrar.php");
+if (!defined('ruta_verificar')) define('ruta_verificar',url_base."proyectoTAPSD/PHP/verificar.php");
 
